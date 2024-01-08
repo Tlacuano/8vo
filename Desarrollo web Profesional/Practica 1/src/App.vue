5384f3203a8f974d300f1eb87239c80fb4579404
Tlacuano <105244719+Tlacuano@users.noreply.github.com>
2024-01-08 17:00:32 -0600

    Add files via upload

---
+++ Desarrollo web Profesional/Practica 1/src/App.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div>
+    <Navbar/>
+    <b-container-fluid>
+
+    </b-container-fluid>
+  </div>
+</template>
+
+<script setup>
+  import Navbar from "./components/navbar.vue"
+</script>
+
+<style scoped>
+
+</style>
